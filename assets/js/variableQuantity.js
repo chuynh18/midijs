4fcc81eedffd455c3091ee9570256f5a6f59eb07
Chris <lamcraft2003@yahoo.com>
2025-07-05 00:55:11 -0700

    greatly simplified delta-time representation conversion logic

--- assets/js/variableQuantity.js
+++ assets/js/variableQuantity.js
@@ -11,9 +11,7 @@ function variableQuantityToValue(byteArray) {
     const byteArrayBinary = byteArray.map(byte => Uint8DecimalToBinary(byte));
     const flatByteArrayBinaryRemoveMsbFromEach = byteArrayBinary.flatMap(byte => byte.slice(1));
     const padded = Array(byteArrayBinary.length).fill(0).concat(flatByteArrayBinaryRemoveMsbFromEach);
-    const unflattened = unflattenUint8Array(padded);
-    const valueArray = unflattened.map(byteArray => parseInt(byteArray.join(""), 2));
-    return convertValueArrayToValue(valueArray);
+    return parseInt(padded.join(""), 2);
 }
 
 /**
@@ -43,31 +41,4 @@ function decimalToBinary(number) {
     return Array
         .from(number.toString(2)) // convert Number to binary (represented as a String) and turn the string into an array
         .map(digit => Number(digit)); // turn each digit from a 1 char String back into a Number
-}
-
-function unflattenUint8Array(flatArray) {
-    const result = [];
-    const byte = [];
-
-    for (let i = 0; i < flatArray.length; i++) {
-        byte.push(flatArray[i]);
-        if (byte.length === 8) {
-            result.push(Array.from(byte));
-            byte.length = 0;
-        }
-    }
-
-    return result;
-}
-
-function convertValueArrayToValue(valueArray) {
-    const NUM_POSSIBLE_VALUES_UINT8 = 256;
-    let answer = 0;
-    let exponent = valueArray.length - 1;
-
-    for (let i = 0; i < valueArray.length; i++) {
-        answer += valueArray[i] * Math.pow(NUM_POSSIBLE_VALUES_UINT8, exponent--);
-    }
-
-    return answer;
 }
